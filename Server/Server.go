9eb23b12667250da55460f1d7c0d63debf78f047
Noah <gafa@itu.dk>
2024-10-24 11:40:20 +0200

    Improved print stuff for server

--- Server/Server.go
+++ Server/Server.go
@@ -53,27 +53,29 @@ func (s *ChatServer) StartServer() {
 }
 
 func (s *ChatServer) JoinChat(user *proto.UserRequest, stream proto.ChatService_JoinChatServer) error {
-	if _, exists := s.clients[user.Username]; exists {
-		log.Printf("User %s has already joined.", user.Username)
-	} else {
-		s.clients[user.Username] = &Client{stream: stream}
-
-		joinMessage := fmt.Sprintf("User %s has joined the chat.", user.Username)
-		log.Printf(joinMessage)
-
-		joinMsg := &proto.Chat{
-			Username:  "Server",
-			Message:   joinMessage,
-			Timestamp: 0,
-		}
-		s.broadcastMessage(joinMsg)
+	_, userAlreadyJoined := s.clients[user.Username]
+	if userAlreadyJoined {
+		log.Printf("User %s has already joined", user.Username)
+		return nil
 	}
+
+	newUserClient := &Client{stream: stream}
+	s.clients[user.Username] = newUserClient
+
+	joinMessage := fmt.Sprintf("User %s has joined the chat", user.Username)
+	log.Print(joinMessage)
+
+	joinMsg := &proto.Chat{
+		Username:  "Server",
+		Message:   joinMessage,
+		Timestamp: -1,
+	}
+	s.broadcastMessage(joinMsg)
 
 	return nil
 }
 
 func (s *ChatServer) BroadcastMessage(ctx context.Context, chat *proto.Chat) (*proto.Empty, error) {
-	log.Printf("%d | %s: %s", chat.Timestamp, chat.Username, chat.Message)
 	s.broadcastMessage(chat)
 
 	return &proto.Empty{}, nil
@@ -81,14 +83,15 @@ func (s *ChatServer) BroadcastMessage(ctx context.Context, chat *proto.Chat) (*p
 
 func (s *ChatServer) LeaveChat(ctx context.Context, user *proto.UserRequest) (*proto.Empty, error) {
 	delete(s.clients, user.Username)
+	// TEST IF CLIENT HAS ACTUALLY BEEN DELETED
 
-	leaveMessage := fmt.Sprintf("User %s left the chat.", user.Username)
-	log.Printf(leaveMessage)
+	leaveMessage := fmt.Sprintf("User %s left the chat", user.Username)
+	log.Print(leaveMessage)
 
 	leaveMsg := &proto.Chat{
 		Username:  "Server",
 		Message:   leaveMessage,
-		Timestamp: 0,
+		Timestamp: -1,
 	}
 	s.broadcastMessage(leaveMsg)
 
@@ -96,11 +99,15 @@ func (s *ChatServer) LeaveChat(ctx context.Context, user *proto.UserRequest) (*p
 }
 
 func (s *ChatServer) broadcastMessage(message *proto.Chat) {
-	for username, connection := range s.clients {
-		if username != message.Username {
-			if err := connection.stream.Send(message); err != nil {
-				log.Printf("Failed to send message to %s | %v", username, err)
-			}
+	log.Printf("Broadcasting message: '%d | %s: %s'", message.Timestamp, message.Username, message.Message)
+	for username, userConnection := range s.clients {
+		log.Printf("Sending to %s", username)
+
+		sendErr := userConnection.stream.Send(message)
+		if sendErr != nil {
+			log.Printf("Failed to send message to %s | %v", username, sendErr)
+			continue
 		}
+		log.Printf("Sent message to %s", username)
 	}
 }
